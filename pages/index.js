f21709977e75699676afc8cc7af4f01ffa65751d
Steven Haye <steven@northwestmediacollective.com>
2022-06-24 09:03:29 -0700

    added form processing route

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,40 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useRef } from 'react'
 
 export default function Home() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+  function submitFormHandler(e) {
+
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+
+  }
   return (
-   <div>
-    <h1>The Home Page</h1>
-   </div> 
+    <div>
+      <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Feedback</label>
+          <textarea id="feedback" rows='5' ref={feedbackInputRef} />
+        </div>
+        <button>Send Feedback</button>
+      </form>
+    </div>
   )
 }
